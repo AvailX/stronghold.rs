414976c52e4b4db80aa235027286f6a11f09f6d9
Zack-Xb <zack.xuereb.21@um.edu.mt>
2024-04-16 21:24:49 +0200

    exports AleoSignRequest

--- client/src/procedures.rs
+++ client/src/procedures.rs
@@ -15,7 +15,7 @@ pub use primitives::{
     BIP39Recover, ConcatKdf, ConcatSecret, CopyRecord, Curve, Ed25519Sign, GarbageCollect, GenerateKey, GetEvmAddress,
     Hkdf, Hmac, KeyType, MnemonicLanguage, Pbkdf2Hmac, PublicKey, RevokeData, Secp256k1EcdsaFlavor, Secp256k1EcdsaSign,
     Sha2Hash, Slip10Chain, Slip10ChainCode, Slip10Derive, Slip10DeriveInput, Slip10Generate, StrongholdProcedure,
-    WriteVault, X25519DiffieHellman, AleoSign, GetAleoAddress
+    WriteVault, X25519DiffieHellman, AleoSign, GetAleoAddress, AleoSignRequest
 };
 pub use types::{
     DeriveSecret, FatalProcedureError, GenerateSecret, Procedure, ProcedureError, ProcedureOutput, UseSecret,
